ffd7fce397f187785101c5d4082b603d60137253
yecanlee <yecanlee@yahoo.com>
2025-06-04 10:43:53 +0000

    [Add]zsseg model with semantic segmentation codes

---
+++ zsseg_baseline/mask_former/data/datasets/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+from . import (
+    register_coco_stuff,
+    register_voc_seg,
+    register_pcontext,
+    register_ceiling,
+    register_ceiling_semantic,
+)
